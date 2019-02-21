588c777801530348017be0fe0ff71e9db2c08cf4
Bhupendra Aole <baole@imminet.com>
2019-02-21 17:17:56 -0500

    some updates
    some updates

--- mota.py
+++ mota.py
@@ -8,36 +8,63 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 
 GAMMA       = 0.99
-BATCH_SIZE  = 32
+BATCH_SIZE  = 64
+MEMORY_SIZE = 1024
 
+def normalize(v):
+    norm=np.linalg.norm(v, ord=1)
+    if norm==0:
+        norm=np.finfo(v.dtype).eps
+    return v/norm
+    
 class Model:
     def __init__(self):
-        output_size = 3*3*2*3
+        self.output_size = 3*3*2*3
+        # probabilities
+        self.prob = [1]*self.output_size
+        self.prob[0] = .1 # discourage standing still
+        # reduce probability of jumping
+        m = 0
+        for i in range(3): #Forward/Back
+            for j in range(3): #Camera
+                for k in range(2):# Jump
+                    for l in range(3):# left/right
+                        if i==1 and j==k==l==0:
+                            self.prob[m] += 150 # encourage moving forward
+                            
+                        m += 1
+        self.prob = normalize(self.prob)
+        #
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.99995
         
         self.memory = []
         
         self.model = Sequential()
     
-        self.model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(168, 168, 3)))
-        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(168, 168, 3)))
+        #self.model.add(Conv2D(64, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         #self.model.add(Dropout(0.25))
 
-        self.model.add(Conv2D(128, (3, 3), activation='relu'))
-        self.model.add(Conv2D(128, (3, 3), activation='relu'))
+        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        #self.model.add(Conv2D(128, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         #self.model.add(Dropout(0.25))
 
         self.model.add(Flatten())
-        self.model.add(Dense(1024, activation='relu'))
+        self.model.add(Dense(512, activation='relu'))
         #self.model.add(Dropout(0.5))
-        self.model.add(Dense(output_size, activation='linear'))
+        self.model.add(Dense(self.output_size, activation='linear'))
 
         self.model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='mse')
         
-    def act(self, env, obs):
-        #action = env.action_space.sample()
-        
+    def act(self, obs, explore=True):
+        if explore and self.epsilon > self.epsilon_min and np.random.random()<self.epsilon:
+            self.epsilon *= self.epsilon_decay
+            return np.random.choice(self.output_size, 1, p=self.prob)[0]
+            
         obs = np.reshape(obs[0], (-1, 168, 168, 3))
         out = self.model.predict(obs)
         action = np.argmax(out[0])
@@ -46,10 +73,24 @@ class Model:
         
     def remember(self, obs, action, reward, obsn, done):
         self.memory.append([obs[0], action, reward, obsn[0], done])
-        if len(self.memory)>256:
+        if len(self.memory)>MEMORY_SIZE:
             del self.memory[0]
         
-    def train(self):
+    def train(self, obs, action, reward, obsn, done):
+        print(f'Training Single:{reward} ->')
+        obs = np.reshape(obs[0], (-1, 168, 168, 3))
+        obsn = np.reshape(obsn[0], (-1, 168, 168, 3))
+        
+        reward_predicted = self.model.predict(obs)
+        Q_sa = self.model.predict(obsn)[0]
+        
+        reward_predicted[0][action] = reward
+        if not done:
+            reward_predicted[0][action] += GAMMA * np.max(Q_sa)
+        self.model.fit(obs, reward_predicted, epochs=10, verbose=0)
+        
+    def train_batch(self):
+        print(f'Training Batch ->')
         experience = random.sample(self.memory, min(len(self.memory), BATCH_SIZE))
 
         obs, actions, rewards, obsn, done = zip(*experience)
@@ -62,6 +103,5 @@ class Model:
             reward_predicted[i][actions[i]] = rewards[i]
             if not done[i]:
                 reward_predicted[i][actions[i]] += GAMMA * np.max(Q_sa[i])
-                
         self.model.fit(obs, reward_predicted, epochs=10, verbose=0)
         
--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from obstacle_tower_env import ObstacleTowerEnv
-import sys
+import sys, os, re
 import argparse
 from mota import Model
+from tqdm import trange
+from PIL import Image
+import numpy as np
+from skimage.measure import compare_ssim
 
 input_map = []
 for i in range(3):
@@ -11,34 +15,97 @@ for i in range(3):
                 input_map.append([i, j, k, l])
                 
 model = Model()
+MAX_EPISODES = 20
 
-def run_episode(env):
-    done = False
+def run_test(env, episode_number):
+    print('Running test!')
     total_reward = 0.0
     obs = env.reset()
     
-    while not done:
+    imdata = obs[0]*255
+    im = [Image.fromarray(imdata.astype(np.uint8))]
+    
+    for i in range(5000): #trange(5000):
         #
-        action = model.act(env, obs)
+        action = model.act(obs, False)
         obsn, reward, done, _ = env.step(input_map[action])
         
-        if reward:
-            print(f'\tReward: {reward}')
+        imdata = obsn[0]*255
+        im.append(Image.fromarray(imdata.astype(np.uint8)))
+    
+        obs = obsn
+        total_reward += reward
+        
+        if done:
+            break
             
+    im[0].save(f'test{episode_number}.gif', save_all=True, append_images=im[1:], duration=200, loop=0)
+
+    return total_reward
+
+def run_episode(env, episode_number, save=True):
+    total_reward = 0.0
+    obs = env.reset()
+    
+    if save:
+        imdata = obs[0]*255
+        im = [Image.fromarray(imdata.astype(np.uint8))]
+    
+    #for i in range(5000): 
+    for i in trange(5000):
+        #
+        action = model.act(obs)
+        obsn, reward, done, _ = env.step(input_map[action])
+        
+        if save:
+            imdata = obsn[0]*255
+            im.append(Image.fromarray(imdata.astype(np.uint8)))
+    
+        # increase reward impact
+        reward *= 10
+        #
+        #if done and reward==0:
+        #    reward = -10
+        
+        # if image same as previous
+        # staring/standing still?
+        #if len(model.memory)>2 and (np.array_equal(model.memory[-1][0], obsn[0]) or np.array_equal(model.memory[-2][0], obsn[0])):
+        if len(model.memory)>2 and (compare_ssim(model.memory[-1][0], obsn[0], multichannel=True)>0.9 or compare_ssim(model.memory[-2][0], obsn[0], multichannel=True)>0.9):
+            reward -= 1
+            
+        if reward!=0:
+            model.train(obs, action, reward, obsn, done)
+            
+        #if not done: # reward for surviving
+            #reward += 0.1
         model.remember(obs, action, reward, obsn, done)
-        model.train()
         
         obs = obsn
         total_reward += reward
         
+        if done:
+            break
+            
+    if save:
+        im[0].save(f'episode{episode_number}.gif', save_all=True, append_images=im[1:], duration=200, loop=0)
+
+    model.train_batch()
+    
     return total_reward
 
 def run_evaluation(env):
     while not env.done_grading():
-        run_episode(env)
+        run_episode(env, 0)
         env.reset()
 
 if __name__ == '__main__':
+    # delete files generated in previous run
+    dir = '.'
+    files = os.listdir(dir)
+    for file in files:
+        if re.search('(episode|test)\d+\.gif', file):
+            os.remove(os.path.join(dir, file))
+            
     parser = argparse.ArgumentParser()
     parser.add_argument('environment_filename', default='./ObstacleTower/obstacletower', nargs='?')
     parser.add_argument('--docker_training', action='store_true')
@@ -55,10 +122,14 @@ if __name__ == '__main__':
         while True:
             episode_number += 1
             print(f"Episode {episode_number}...")
-            episode_reward = run_episode(env)
-            print(f"...reward: {episode_reward}")
+            episode_reward = run_episode(env, episode_number, episode_number%5==0 or episode_number==1)
+            print(f"...reward: {episode_reward}. Epsilon: {model.epsilon}")
             
-            if episode_number>20:
+            if episode_number%10==0:
+                episode_reward = run_test(env, episode_number)
+                print(f'...test reward:{episode_reward}.')
+                
+            if episode_number>=MAX_EPISODES:
                 break
 
     env.close()

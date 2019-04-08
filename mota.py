572467b0d3d39f8b93c2bdd5c484487a9ebf3f58
Bhupendra Aole <baole@imminet.com>
2019-04-08 09:31:53 -0400

    update

--- mota.py
+++ mota.py
@@ -7,10 +7,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 
-GAMMA       = 0.99
-BATCH_SIZE  = 64
-BATCH_EPOCH = 1
-MEMORY_SIZE = 2024
+np.random.seed(4)
+tf.set_random_seed(4)
+tf.logging.set_verbosity(tf.logging.INFO)
 
 ACTION_MAP = (
     [0,0,0,0], # frozen
@@ -23,8 +22,27 @@ ACTION_MAP = (
     )
 #ACTION_SIZE = 3*3*2*3
 ACTION_SIZE = len(ACTION_MAP)
+IMAGE_SHAPE = (84,84,3)
+TF_IMAGE_SHAPE = (1,84,84,3)
 
-USE_ADVANTAGE = True
+GAMMA       = 0.99
+BATCH_SIZE  = 64
+BATCH_EPOCH = 1
+BATCH_LOOP = 10
+MEMORY_SIZE = 2024
+
+USE_ADVANTAGE = False
+SPREAD_REWARD = False
+SPREAD_REWARD_DECAY = 0.9
+LEARNING_RATE = 0.01
+USE_ACTION_PROBABILITIES = True
+
+ACTION_HACKS = True
+MOVE_FORWARD_PROB = 2
+CAMERA_PROB = .9
+JUMP_PROB = .7
+
+DENSE_LAYER_SIZE = 512
 
 def normalize(v):
     norm=np.linalg.norm(v, ord=1)
@@ -32,27 +50,28 @@ def normalize(v):
         norm=np.finfo(v.dtype).eps
     return v/norm
     
+def softmax(v):
+    x = v[0]
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+    
+class Actor:
+    def __init__(self):
+        self.s = tf.placeholder(tf.float32, TF_IMAGE_SHAPE, "state")
+    
+class Critic:
+    def __init__(self):
+        pass
+    
 class Model:
     def __init__(self):
-        np.random.seed(4)
         # probabilities
         self.prob = [1]*ACTION_SIZE
-        self.prob[1] = 2
-        self.prob[5] = self.prob[6] = .2 # jump
-        self.prob[3] = self.prob[4] = .5 # camera
-        '''
-        self.prob[0] = .1 # discourage standing still
-        # reduce probability of jumping
-        m = 0
-        for i in range(3): #Forward/Back
-            for j in range(3): #Camera
-                for k in range(2):# Jump
-                    for l in range(3):# left/right
-                        if i==1 and j==k==l==0:
-                            self.prob[m] += 150 # encourage moving forward
-                            
-                        m += 1
-        '''
+        if ACTION_HACKS:
+            self.prob[1] = MOVE_FORWARD_PROB
+            self.prob[5] = self.prob[6] = CAMERA_PROB
+            self.prob[3] = self.prob[4] = CAMERA_PROB
+            
         self.prob = normalize(self.prob)
         #
         self.epsilon = 1.0  # exploration rate
@@ -63,7 +82,7 @@ class Model:
         
         self.model = Sequential()
     
-        self.model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(168, 168, 3)))
+        self.model.add(Conv2D(32, (3, 3), activation='relu', input_shape=IMAGE_SHAPE))
         #self.model.add(Conv2D(64, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         #self.model.add(Dropout(0.25))
@@ -73,32 +92,57 @@ class Model:
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         #self.model.add(Dropout(0.25))
 
+        self.model.add(Conv2D(128, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+            
         self.model.add(Flatten())
-        self.model.add(Dense(512, activation='relu'))
+        self.model.add(Dense(DENSE_LAYER_SIZE, activation='relu'))
         #self.model.add(Dropout(0.5))
         self.model.add(Dense(ACTION_SIZE, activation='linear'))
 
-        self.model.compile(optimizer=tf.train.AdamOptimizer(0.001), loss='mse')
+        self.model.compile(optimizer=tf.train.AdamOptimizer(LEARNING_RATE), loss='mse')
         
     def act(self, obs, explore=True):
         if explore and self.epsilon > self.epsilon_min and np.random.random()<self.epsilon:
             self.epsilon *= self.epsilon_decay
             return np.random.choice(ACTION_SIZE, 1, p=self.prob)[0]
             
-        obs = np.reshape(obs[0], (-1, 168, 168, 3))
+        obs = np.reshape(obs[0], (-1, IMAGE_SHAPE[0], IMAGE_SHAPE[1], IMAGE_SHAPE[2]))
         out = self.model.predict(obs)
-        action = np.argmax(out[0])
+        p = softmax(out)
+        
+        if USE_ACTION_PROBABILITIES:
+            action = np.random.choice(ACTION_SIZE, 1, p=p)[0]
+        else:
+            action = np.argmax(out[0])
         
         return action #input_map[action]
         
     def remember(self, obs, action, reward, obsn, done):
-        self.memory.append([obs[0], action, reward, obsn[0], done])
+        self.memory.append([obs, action, reward, obsn, done])
         if len(self.memory)>MEMORY_SIZE:
-            del self.memory[0]
+            found = False
+            # delete memories that are not relevant
+            for i in range(len(self.memory)):
+                if abs(self.memory[i][2]) < 1:
+                    del self.memory[i]
+                    found = True
+                    break
+            # if all are relevant, delete the oldest
+            if not found:
+                del self.memory[0]
         
+        if SPREAD_REWARD and reward!=0 and len(self.memory)>2:
+            r = reward
+            for i in range(-2, -len(self.memory), -1):
+                r *= SPREAD_REWARD_DECAY
+                if r<=0.01:
+                    break
+                self.memory[i][2] += r
+                
     def train(self, obs, action, reward, obsn, done):
-        obs = np.reshape(obs[0], (-1, 168, 168, 3))
-        obsn = np.reshape(obsn[0], (-1, 168, 168, 3))
+        obs = np.reshape(obs[0], (-1, IMAGE_SHAPE[0], IMAGE_SHAPE[1], IMAGE_SHAPE[2]))
+        obsn = np.reshape(obsn[0], (-1, IMAGE_SHAPE[0], IMAGE_SHAPE[1], IMAGE_SHAPE[2]))
         
         reward_predicted = self.model.predict(obs)
         Q_sa = self.model.predict(obsn)[0]
@@ -113,21 +157,54 @@ class Model:
         self.model.fit(obs, reward_predicted, epochs=10, verbose=0)
         
     def train_batch(self):
-        experience = random.sample(self.memory, min(len(self.memory), BATCH_SIZE))
+        h = []
+        for j in range(BATCH_LOOP):
+            experience = random.sample(self.memory, min(len(self.memory), BATCH_SIZE))
 
-        obs, actions, rewards, obsn, done = zip(*experience)
-        obs = np.asarray(obs)
+            obs, actions, rewards, obsn, done = zip(*experience)
+            obs = np.asarray(obs)
+            
+            reward_predicted = self.model.predict(obs)
+            Q_sa = self.model.predict(np.asarray(obsn))
+            
+            for i, ex in enumerate(experience):
+                reward_predicted[i][actions[i]] = rewards[i]
+                if not done[i]:
+                    reward_predicted[i][actions[i]] += GAMMA * np.max(Q_sa[i])
+                    
+            hist = self.model.fit(obs, reward_predicted, epochs=BATCH_EPOCH, verbose=0)
+            h.append(hist.history['loss'])
         
-        reward_predicted = self.model.predict(obs)
-        Q_sa = self.model.predict(np.asarray(obsn))
-        
-        for i, ex in enumerate(experience):
-            reward_predicted[i][actions[i]] = rewards[i]
-            if not done[i]:
-                reward_predicted[i][actions[i]] += GAMMA * np.max(Q_sa[i])
-        self.model.fit(obs, reward_predicted, epochs=BATCH_EPOCH, verbose=0)
-        
-        
+        return np.mean(h)
+                
 if __name__ == '__main__':
     model = Model()
+    
+    for i, l in enumerate(model.model.layers):
+        w = l.get_weights()
+        if type(w)==list:
+            for j,k in enumerate(w):
+                print(i, j, k.shape, np.max(k))
+        else:
+            print(i, w.shape, np.max(w))
+            
+    for t in range(100):
+        for i in range(10):
+            obs = np.random.random((1,IMAGE_SHAPE[0], IMAGE_SHAPE[1], IMAGE_SHAPE[2]))
+            model.act(obs)
+            obsn = np.random.random((IMAGE_SHAPE[0], IMAGE_SHAPE[1], IMAGE_SHAPE[2]))
+            a = np.random.choice(ACTION_SIZE, 1)[0]
+            r = np.random.choice(10, 1)[0]
+            model.remember(obs[0], a, r, obsn, False)
+        print('training...')
+        model.train_batch()
+        
+        for i, l in enumerate(model.model.layers):
+            w = l.get_weights()
+            if type(w)==list:
+                for j,k in enumerate(w):
+                    print(i, j, k.shape, np.max(k))
+            else:
+                print(i, w.shape, np.max(w))
+            
     print('done!')
--- run.py
+++ run.py
@@ -1,112 +1,137 @@
 from obstacle_tower_env import ObstacleTowerEnv
-import sys, os, re
+import sys, os, re, time, random
 import argparse
-from mota import Model, ACTION_MAP
+from mota import Model, ACTION_MAP, Actor, Critic
 from tqdm import trange
 from PIL import Image
 import numpy as np
 from skimage.measure import compare_ssim
+import logging
+from skimage.transform import resize
 
-'''
-input_map = []
-for i in range(3):
-    for j in range(3):
-        for k in range(2):
-            for l in range(3):
-                input_map.append([i, j, k, l])
-'''
+logger = logging.getLogger("gym_unity")
 
-MAX_EPISODES = 50
-REWARD_MULTIPLIER = 10
+MAX_EPISODES = 10000
 EPISODE_DONE_PENALTY = False
-COMPARE_PREVIOUS_FRAMES = True
+COMPARE_PREVIOUS_FRAMES = False
 TRAIN_REWARD = False
+STEP_REWARD = 0
+TRAIN_BATCH_EVERY_STEP = False
+ACTION_REWARD_HACKS = (0,0,0,0,0,0,0)
+TRAIN_EVERY = 1
 
 model = Model()
+actor = Actor()
+critic = Critic()
 
-def run_test(env, episode_number):
-    print('Running test!')
+fastest_to_floor = [10000, 10000, 10000, 10000, 10000, 10000, 10000]
+
+def run_episode(env, episode_number, test=False):
+    starting_floor = random.choice([0,1])
+    if test:
+        print(f'TST: {episode_number}-{starting_floor}...')
+    else:
+        print(f'EPS: {episode_number}-{starting_floor}...')
+        
     total_reward = 0.0
+    
+    env.floor(starting_floor)
+    num_floors = starting_floor
+    
     obs = env.reset()
+    
+    obs_im = resize(obs[0], (84,84,3), anti_aliasing=False, mode='constant')
     
     imdata = obs[0]*255
     im = [Image.fromarray(imdata.astype(np.uint8))]
     
-    for i in range(50000): #trange(5000):
+    for frames in range(50000):
+    #for i in trange(5000):
         #
-        action = model.act(obs, False)
-        obsn, reward, done, _ = env.step(ACTION_MAP[action])
+        action = model.act(obs, test==False)
+        repeat_action = 1
+        if action in [3,4]:
+            repeat_action = 5
         
+        for ra in range(repeat_action):
+            obsn, reward, done, _ = env.step(ACTION_MAP[action])
+            
+        obsn_im = resize(obs[0], (84,84,3), anti_aliasing=False)
+        if reward==1:
+            num_floors += 1
+            next_floor_found = True
+            if frames<fastest_to_floor[num_floors]:
+                fastest_to_floor[num_floors] = frames
+            reward = 10
+                
         imdata = obsn[0]*255
         im.append(Image.fromarray(imdata.astype(np.uint8)))
     
-        obs = obsn
+        if not test:
+            
+            # reward for taking certain actions
+            reward += ACTION_REWARD_HACKS[action]
+            
+            # if image same as previous
+            # staring/standing still?
+            
+            if COMPARE_PREVIOUS_FRAMES:
+                if len(model.memory)>2 and (compare_ssim(model.memory[-1][0], obsn_im, multichannel=True)>0.95 or compare_ssim(model.memory[-2][0], obsn_im, multichannel=True)>0.95):
+                    reward -= 1
+            
+            # reward for every step
+            reward += STEP_REWARD
+            
+            if done and not num_floors>starting_floor:
+                if EPISODE_DONE_PENALTY:
+                    reward -= 1
+                
+            model.remember(obs_im, action, reward, obsn_im, done)
+            
+            if round(reward)!=0:
+                print('\t'+'* '*num_floors +f'Reward: {reward} ({frames})', end='')
+                if TRAIN_REWARD:
+                    print(' Training...', end='')
+                    st = time.time()
+                    model.train_batch()
+                    print(f'{round(time.time()-st, 2)}!')
+                else:
+                    print()
+            if TRAIN_BATCH_EVERY_STEP:
+                print(f'\tBatch: Floors:{num_floors}.')
+                model.train_batch()
+                
+            obs = obsn
+        
         total_reward += reward
         
         if done:
             break
             
-    im[0].save(f'test{episode_number}.gif', save_all=True, append_images=im[1:], duration=200, loop=0)
+    if test:
+        im[0].save(f'test{episode_number}_{starting_floor}-{num_floors}.gif', save_all=True, append_images=im[1:], duration=200, loop=0)
+    elif num_floors>starting_floor:
+        im[0].save(f'episode{episode_number}_{starting_floor}-{num_floors}.gif', save_all=True, append_images=im[1:], duration=200, loop=0)
 
-    return total_reward
-
-def run_episode(env, episode_number, save=True):
-    total_reward = 0.0
-    obs = env.reset()
-    
-    if save:
-        imdata = obs[0]*255
-        im = [Image.fromarray(imdata.astype(np.uint8))]
-    
-    num_floors = 1
-    
-    for i in range(50000):
-    #for i in trange(5000):
-        #
-        action = model.act(obs)
-        obsn, reward, done, _ = env.step(ACTION_MAP[action])
-        if reward==1:
-            num_floors += 1
-            
-        if save:
-            imdata = obsn[0]*255
-            im.append(Image.fromarray(imdata.astype(np.uint8)))
-    
-        # increase reward impact
-        reward *= REWARD_MULTIPLIER
+    if not TRAIN_BATCH_EVERY_STEP and not test:
+        print(f'\tLvl: +{num_floors-starting_floor} ({round(total_reward,3)}). Eps:{round(model.epsilon,3)}...', end='')
+        if episode_number % TRAIN_EVERY==0:
+            st = time.time()
+            loss = model.train_batch()
+            '''
+            if loss>10:
+                model.epsilon = max(0.1, model.epsilon-0.1)
+            '''
+            if loss<0.01:
+                model.epsilon = min(0.9, model.epsilon+0.1)
+            print(f'L:{round(loss,5)} T:{round(time.time()-st, 2)}')
+        else:
+            print('.')
+    elif test:
+        print(f'\tLvl: +{num_floors-starting_floor} ({frames}).')
         
-        if EPISODE_DONE_PENALTY:
-            if done and reward==0:
-                reward = -10
-        
-        # if image same as previous
-        # staring/standing still?
-        if COMPARE_PREVIOUS_FRAMES:
-            if len(model.memory)>2 and (compare_ssim(model.memory[-1][0], obsn[0], multichannel=True)>0.95 or compare_ssim(model.memory[-2][0], obsn[0], multichannel=True)>0.95):
-                reward -= 1
-            
-        if reward!=0 and TRAIN_REWARD:
-            print(f'\tTraining Single:{reward}')
-            model.train(obs, action, reward, obsn, done)
-            
-        #if not done: # reward for surviving
-            #reward += 0.1
-        model.remember(obs, action, reward, obsn, done)
-        
-        obs = obsn
-        total_reward += reward
-        
-        if done:
-            break
-            
-    if save:
-        im[0].save(f'episode{episode_number}.gif', save_all=True, append_images=im[1:], duration=200, loop=0)
-
-    print(f'\tTraining Batch: Floors:{num_floors}.')
-    model.train_batch()
+    return frames
     
-    return total_reward
-
 def run_evaluation(env):
     while not env.done_grading():
         run_episode(env, 0)
@@ -117,7 +142,7 @@ if __name__ == '__main__':
     dir = '.'
     files = os.listdir(dir)
     for file in files:
-        if re.search('(episode|test)\d+\.gif', file):
+        if re.search('^(episode|test|frame).+\.(gif|jpg)$', file):
             os.remove(os.path.join(dir, file))
             
     parser = argparse.ArgumentParser()
@@ -126,24 +151,22 @@ if __name__ == '__main__':
     parser.set_defaults(docker_training=False)
     args = parser.parse_args()
 
-    print('opening env...')
     env = ObstacleTowerEnv(args.environment_filename, docker_training=args.docker_training, retro=False, realtime_mode=False)
+    logger.setLevel(logging.WARNING)
     env.seed(4)
     
     if env.is_grading():
         episode_reward = run_evaluation(env)
     else:
-        episode_reward = 0
+        total_frames = 0
         episode_number = 0
         while True:
             episode_number += 1
-            print(f"Episode {episode_number}...")
-            episode_reward = run_episode(env, episode_number, episode_number%5==0 or episode_number==1)
-            print(f"...reward: {round(episode_reward,5)}. Epsilon: {round(model.epsilon,5)}")
+            total_frames += run_episode(env, episode_number)
             
-            if episode_number%10==0:
-                episode_reward = run_test(env, episode_number)
-                print(f'...test reward:{round(episode_reward,5)}.')
+            if episode_number%200==0:
+                print(f'Total Frames: {total_frames}')
+                episode_reward = run_episode(env, episode_number, test=True)
                 
             if episode_number>=MAX_EPISODES:
                 break
